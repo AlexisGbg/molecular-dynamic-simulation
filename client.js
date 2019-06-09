b533cb56dfc2fa45d1c405433e5b7b7eae3e1efd
Alexis Macbook <alexguibourge@gmail.com>
2019-06-09 15:18:01 +0200

    resolve asynchronous issues

--- client.js
+++ client.js
@@ -1,10 +1,19 @@
-
-const collisionSystem = new window.CollisionSystem("system", 50); 
-
-// MAIN LOOP
-window.setInterval(
-    () => collisionSystem.simulate(),
-    1);
-
-
-
+function loadScript(url) {
+   return new Promise((resolve, reject) => {
+	const head   = document.head;
+	const script = document.createElement('script');
+	script.type = 'module';
+	script.src = url;
+        script.onload = () => {resolve();}; 
+	head.appendChild(script);
+    });
+};
+		      
+loadScript('./CollisionSystem.js')
+    .then((elt) =>
+    	  {console.log(window.CollisionSystem); 
+    	   var collisionSystem = new window.CollisionSystem("system", 50);
+    	   window.setInterval(
+    	       () => collisionSystem.simulate(),
+    	       1);
+    	  }); 

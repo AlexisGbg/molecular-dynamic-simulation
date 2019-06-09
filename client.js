73a43d20fe98eeefff3752e0a81a18a47755dd0b
Alexis Macbook <alexguibourge@gmail.com>
2019-06-09 18:19:34 +0200

    add scrolling handling

--- client.js
+++ client.js
@@ -1,19 +1,47 @@
-function loadScript(url) {
+function loadScript(url, type) {
    return new Promise((resolve, reject) => {
 	const head   = document.head;
 	const script = document.createElement('script');
-	script.type = 'module';
+	script.type = type;
 	script.src = url;
         script.onload = () => {resolve();}; 
 	head.appendChild(script);
     });
 };
-		      
-loadScript('./CollisionSystem.js')
+
+
+// Ensure the needed scripts are loaded before simulating
+Promise.all([
+    loadScript('https://d3js.org/d3.v4.min.js', 'text/javascript'),
+    loadScript('https://alexis-guibourge.blog:9090/CollisionSystem.js', 'module')])
     .then((elt) =>
-    	  {console.log(window.CollisionSystem); 
-    	   var collisionSystem = new window.CollisionSystem("system", 50);
-    	   window.setInterval(
-    	       () => collisionSystem.simulate(),
-    	       1);
-    	  }); 
+    	  {
+    	      window.collisionSystem = new window.CollisionSystem('system', 50);
+    	      window.collisionSystemTimer = setInterval(() => collisionSystem.simulate(),1);
+	      window.collisionSystemOn = true; 
+	      window.dispatchEvent(new Event("wheel")); // Force to check the initial position of the system.
+	  }
+	 ); 
+
+// Stops the simulation if the system is not on the screen
+window.addEventListener(
+    "wheel",
+    () => {
+	const containerPos = document.getElementById("system").getBoundingClientRect();
+	const top = containerPos.top;
+	const bottom = containerPos.bottom;
+	const screenHeight = window.innerHeight;
+	if ((top < 0 || bottom > screenHeight) && collisionSystemOn) {
+	    clearInterval(collisionSystemTimer);
+	    window.collisionSystemOn = false; 
+	} else if ((top > 0 && bottom < screenHeight) && !collisionSystemOn) {
+    	    window.collisionSystemTimer = setInterval(() => collisionSystem.simulate(),1);
+	    window.collisionSystemOn = true; 
+	}
+    }); 
+
+
+
+
+
+
